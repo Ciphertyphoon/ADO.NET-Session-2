c835dff17ea2c9aaaa60c6ad24efb05713eb0179
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-06-16 15:04:52 -0400

    Connections

---
+++ Sample ADO.NetWebApplication/ConnectionStrings.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sample_ADO.NetWebApplication
+{
+    public partial class ConnectionStrings : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String ConStr = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(ConStr);
+            try
+            {
+                String Query = "SELECT CustomerId, ContactName, City, Country FROM Customers";
+                SqlCommand cmd = new SqlCommand(Query, con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                GridView1.DataSource = rdr;
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception:" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    }
+}

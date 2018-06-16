796acd48f26a15db11d05d68b337054be71f2ce3
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-06-16 15:23:19 -0400

    Connects with App.config

--- ConsoleAppADO.net/Program.cs
+++ ConsoleAppADO.net/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,30 @@ namespace ConsoleAppADO.net
     {
         static void Main(string[] args)
         {
+            String ConStr = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+            SqlConnection con = new SqlConnection(ConStr);
+            try
+            {
+                String Query = "SELECT CustomerId, ContactName, City, Country FROM Customers";
+                SqlCommand cmd = new SqlCommand(Query, con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr["CustomerId"] + " " + rdr["ContactName"] + " " + rdr["City"] + " " + rdr["Country"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hi there! this peogram arises Exception:" + ex);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+
+            }
         }
     }
 }

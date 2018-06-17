b8e6acf0d8777d479bd8e845342b607d22ebfacf
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-06-17 15:10:57 -0400

    command execute, scalar, non query

---
+++ Sample ADO.NetWebApplication/SQLCommand.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SQLCommand.aspx.cs" Inherits="Sample_ADO.NetWebApplication.SQLCommand" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <%--<asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#E7E7FF" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Horizontal">
+                <AlternatingRowStyle BackColor="#F7F7F7" />
+                <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
+                <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+                <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+                <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+                <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+                <sortedascendingcellstyle backcolor="#F4F4FD" />
+                <sortedascendingheaderstyle backcolor="#5A4C9D" />
+                <sorteddescendingcellstyle backcolor="#D8D8F0" />
+                <sorteddescendingheaderstyle backcolor="#3E3277" />
+            </asp:GridView>--%>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Sample ADO.NetWebApplication/SQLCommand.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sample_ADO.NetWebApplication
+{
+    public partial class SQLCommand : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            String ConStr = ConfigurationManager.ConnectionStrings["SilverCyanide"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(ConStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                //cmd.CommandText = "SELECT * FROM tblProducts";
+                //cmd.Connection = con;
+                //con.Open();
+                //SqlDataReader rdr = cmd.ExecuteReader();
+                //GridView1.DataSource = rdr;
+                //GridView1.DataBind();
+
+                //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                //cmd.CommandText = "SELECT COUNT(Product_Name) FROM tblProducts";
+                //cmd.Connection = con;
+                //con.Open();
+                //int TotalRows = (int)cmd.ExecuteScalar();
+                //Response.Write("Total Products:" + TotalRows.ToString());
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                //Create an instance of SqlCommand class, specifying the T-SQL command 
+                //that we want to execute, and the connection object.
+
+                cmd.CommandText = "insert into tblProducts values (103, 'Apple Laptops', 100.00, 50000)";
+                cmd.Connection = con;
+                con.Open();
+
+                //Since we are performing an insert operation, use ExecuteNonQuery() 
+                //method of the command object. ExecuteNonQuery() method returns an 
+                //integer, which specifies the number of rows inserted
+                int rowsAffected = cmd.ExecuteNonQuery();
+                Response.Write("Inserted Rows = " + rowsAffected.ToString() + "<br/>");
+
+                //Set to CommandText to the update query. We are reusing the command object, 
+                //instead of creating a new command object
+                cmd.CommandText = "update tblProducts set QTY_Availabilty = 1010 where ID = 1";
+
+                //use ExecuteNonQuery() method to execute the update statement on the database
+                rowsAffected = cmd.ExecuteNonQuery();
+                Response.Write("Updated Rows = " + rowsAffected.ToString() + "<br/>");
+
+                //Set to CommandText to the delete query. We are reusing the command object, 
+                //instead of creating a new command object
+                cmd.CommandText = "Delete from tblProducts where Product_ID = 102";
+                //use ExecuteNonQuery() method to delete the row from the database
+                rowsAffected = cmd.ExecuteNonQuery();
+                Response.Write("Deleted Rows = " + rowsAffected.ToString() + "<br/>");
+
+            }
+        }
+    }
+}

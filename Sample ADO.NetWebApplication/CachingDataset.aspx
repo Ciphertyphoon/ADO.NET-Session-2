334de8b92c18e1213b779146b6d2862af9fbc5e5
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-06-30 16:49:35 -0400

    Dataset and Caching

---
+++ Sample ADO.NetWebApplication/CachingDataset.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CachingDataset.aspx.cs" Inherits="Sample_ADO.NetWebApplication.CachingDataset" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnLoadData" runat="server" Text="Load Data" OnClick="btnLoadData_Click" />
+            &nbsp;
+            <asp:Button ID="btnClearnCache" runat="server" Text="Clear Cache" OnClick="btnClearnCache_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lblMessage" runat="server"></asp:Label>
+            <br />
+            ----------------------------------------------------------------<asp:GridView ID="gvProducts" runat="server" BackColor="LightGoldenrodYellow" BorderColor="Tan" BorderWidth="1px" CellPadding="2" ForeColor="Black" GridLines="None">
+                <AlternatingRowStyle BackColor="PaleGoldenrod" />
+                <FooterStyle BackColor="Tan" />
+                <HeaderStyle BackColor="Tan" Font-Bold="True" />
+                <PagerStyle BackColor="PaleGoldenrod" ForeColor="DarkSlateBlue" HorizontalAlign="Center" />
+                <SelectedRowStyle BackColor="DarkSlateBlue" ForeColor="GhostWhite" />
+                <SortedAscendingCellStyle BackColor="#FAFAE7" />
+                <SortedAscendingHeaderStyle BackColor="#DAC09E" />
+                <SortedDescendingCellStyle BackColor="#E1DB9C" />
+                <SortedDescendingHeaderStyle BackColor="#C2A47B" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Sample ADO.NetWebApplication/DataSetASP.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sample_ADO.NetWebApplication
+{
+    public partial class DataSetASP : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = ConfigurationManager.ConnectionStrings["SilverCyanide"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("spGetProductAndCategoriesData", connection);
+                dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataSet dataset = new DataSet();
+                dataAdapter.Fill(dataset);
+
+
+                //GridView1.DataSource = dataset;
+                //GridView1.DataBind();
+
+                //GridView2.DataSource = dataset;
+                //GridView2.DataBind();
+
+                //GridView1.DataSource = dataset.Tables[0];
+                //GridView1.DataBind();
+
+                //GridView2.DataSource = dataset.Tables[1];
+                //GridView2.DataBind();
+
+                //By default the tables in the DataSet will have table names as Table, Table1, Table2 etc. So if you want to give the tables in the DataSet a meaningful name, use the TableName property as shown below.
+                dataset.Tables[0].TableName = "Products";
+                dataset.Tables[1].TableName = "Categories";
+
+                //These table names can then be used when binding to a GridView control, instead of using the integral indexer, which makes your code more readable, and maintainable.
+                GridView1.DataSource = dataset.Tables["Products"];
+                GridView1.DataBind();
+
+                GridView2.DataSource = dataset.Tables["Categories"];
+                GridView2.DataBind();
+
+            }
+
+        }
+    }
+}

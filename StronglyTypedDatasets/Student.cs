92c5a4055474c7bf77621bbd62a57171abe3b430
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-07-08 20:25:00 -0400

    Untyped Datasets

---
+++ StronglyTypedDatasets/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StronglyTypedDatasets
+{
+    public class Student
+    {
+
+        private int iD;
+        private string name;
+        private string gender;
+        private int totalMarks;
+
+        public int ID { get => iD; set => iD = value; }
+        public string Name { get => name; set => name = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public int TotalMarks { get => totalMarks; set => totalMarks = value; }
+    }
+
+}
